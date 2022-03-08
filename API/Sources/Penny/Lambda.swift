3221f0eac4b54c6634790d3393dff9c9ae7e26d8
De Bock Benny <bdebock@protonmail.com>
2022-03-08 16:51:49 +0100

    Change lambda name and functionality

--- API/Sources/Penny/Lambda.swift
+++ API/Sources/Penny/Lambda.swift
@@ -1,7 +1,8 @@
 import AWSLambdaRuntime
 
 struct Request: Codable {
-    let name: String
+    let value: Int
+    let receiver: String
 }
 
 struct Response: Codable {
@@ -11,7 +12,7 @@ struct Response: Codable {
 // in this example we are receiving and responding with codables. Request and Response above are examples of how to use
 // codables to model your request and response objects
 @main
-struct MyLambda: LambdaHandler {
+struct AddCoins: LambdaHandler {
     typealias Event = Request
     typealias Output = Response
 
@@ -21,6 +22,6 @@ struct MyLambda: LambdaHandler {
 
     func handle(_ event: Request, context: LambdaContext) async throws -> Response {
         // as an example, respond with the input event's reversed body
-        Response(body: "Hello \(event.name)")
+        Response(body: "\(event.receiver) now has \(event.value) coins")
     }
 }
