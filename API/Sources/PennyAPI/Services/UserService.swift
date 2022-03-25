28e83b4b894ed5d58996f07501ce3c31e0627903
De Bock Benny <bdebock@protonmail.com>
2022-03-25 17:35:25 +0100

    Remove local endpoint

--- API/Sources/PennyAPI/Services/UserService.swift
+++ API/Sources/PennyAPI/Services/UserService.swift
@@ -14,8 +14,8 @@ struct UserService {
     
     init(_ awsClient: AWSClient, _ logger: Logger) {
         let euWest = Region(awsRegionName: "eu-west-2")
-        let endpoint = "http://localhost:8091/"
-        let dynamoDB = DynamoDB(client: awsClient, region: euWest, endpoint: endpoint)
+//        let endpoint = "http://localhost:8091/"
+        let dynamoDB = DynamoDB(client: awsClient, region: euWest/* endpoint: endpoint*/)
         self.logger = logger
         self.userRepo = DynamoUserRepository(db: dynamoDB, tableName: "penny-bot-table", eventLoop: awsClient.eventLoopGroup.next(), logger: logger)
     }
