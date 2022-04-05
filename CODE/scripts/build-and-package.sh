3bbe40c74fbaeae1f0b2fe4064bf9f0c759be97d
De Bock Benny <bdebock@protonmail.com>
2022-04-05 12:56:22 +0200

    update CI and build/package scripts

---
+++ CODE/scripts/build-and-package.sh
@@ -0,0 +1,31 @@
+set -eu
+
+workspace="$(pwd)"
+
+for arg in "$@"
+do
+executable=$arg
+
+echo "---------------------------------------"
+echo "preparing docker build image"
+echo "---------------------------------------"
+docker build . -t builder
+echo "done"
+
+
+echo "---------------------------------------"
+echo "building \"$executable\" lambda in workspace $workspace"
+echo "---------------------------------------"
+docker run --rm -v "$workspace":/workspace -w /workspace builder \
+    bash -cl "swift build --product $executable -c release -Xswiftc -static-stdlib"
+echo "done"
+
+
+echo "---------------------------------------"
+echo "packaging \"$executable\" lambda"
+echo "---------------------------------------"
+docker run --rm -v "$workspace":/workspace -w /workspace builder \
+    bash -cl "./scripts/package.sh $executable"
+echo "done" 
+
+done
