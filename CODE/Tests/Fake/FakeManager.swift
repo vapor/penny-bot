575abaab2e330be5d35a1d87fd05450c2c7453e6
Mahdi Bahrami <github@mahdibm.com>
2022-10-16 13:37:14 +0330

    final fixes + added more tests

--- CODE/Tests/Fake/FakeManager.swift
+++ CODE/Tests/Fake/FakeManager.swift
@@ -1,5 +1,7 @@
 @testable import DiscordBM
+@testable import PennyBOT
 import AsyncHTTPClient
+import Logging
 import Foundation
 import XCTest
 
@@ -13,10 +15,12 @@ private let testData: [String: Any] = {
 }()
 
 /// Probably could be more efficient than encoding then decoding again?!
-func testData(for key: String) -> Data {
-    try! JSONSerialization.data(
-        withJSONObject: testData[key]!
-    )
+func testData(for key: String) -> Data? {
+    if let object = testData[key] {
+        return try! JSONSerialization.data(withJSONObject: object)
+    } else {
+        return nil
+    }
 }
 
 public enum EventKey: String {
@@ -29,28 +33,28 @@ public actor FakeManager: GatewayManager {
     public nonisolated let client: any DiscordClient = FakeDiscordClient()
     public nonisolated let id = 0
     public nonisolated let state: GatewayState = .connected
-    var eventHandlers: [(Gateway.Event) -> Void] = []
     
-    private init() { }
-    public static let shared = FakeManager()
+    public init() { }
+    public static var shared = FakeManager()
     
     public nonisolated func connect() { }
     public func requestGuildMembersChunk(payload: Gateway.RequestGuildMembers) async { }
-    public func addEventHandler(_ handler: @escaping (Gateway.Event) -> Void) async {
-        eventHandlers.append(handler)
-    }
+    public func addEventHandler(_ handler: @escaping (Gateway.Event) -> Void) async { }
     public func addEventParseFailureHandler(
         _ handler: @escaping (Error, String) -> Void
     ) async { }
     public nonisolated func disconnect() { }
     
     public func send(key: EventKey) {
-        let data = testData(for: key.rawValue)
+        let data = testData(for: key.rawValue)!
         let decoder = JSONDecoder()
         let event = try! decoder.decode(Gateway.Event.self, from: data)
-        for handler in eventHandlers {
-            handler(event)
-        }
+        EventHandler(
+            event: event,
+            discordClient: FakeDiscordClient(),
+            coinService: FakeCoinService(),
+            logger: Logger(label: "Test_EventHandler")
+        ).handle()
     }
     
     public func sendAndAwaitResponse<T>(
@@ -60,10 +64,18 @@ public actor FakeManager: GatewayManager {
         file: String = #file,
         line: UInt = #line
     ) async throws -> T {
-        self.send(key: key)
         let value = await withCheckedContinuation { cont in
-            Task { continuations[endpoint.urlSuffix] = cont }
+            Task {
+                continuations[endpoint.urlSuffix] = cont
+                self.send(key: key)
+                try await Task.sleep(nanoseconds: 3_000_000_000)
+                if continuations.removeValue(forKey: endpoint.urlSuffix) != nil {
+                    XCTFail("Penny did not respond in-time to \(endpoint.urlSuffix)")
+                    cont.resume(with: .success(Optional<Never>.none as Any))
+                }
+            }
         }
+
         let unwrapped = try XCTUnwrap(
             value as? T,
             "Value \(value) can't be cast to \(_typeName(T.self))."
@@ -74,7 +86,9 @@ public actor FakeManager: GatewayManager {
     private var continuations = [String: CheckedContinuation<Any, Never>]()
     
     fileprivate func respond(to endpoint: Endpoint, with payload: Any) {
-        continuations.removeValue(forKey: endpoint.urlSuffix)?.resume(returning: payload)
+        if let cont = continuations.removeValue(forKey: endpoint.urlSuffix) {
+            cont.resume(returning: payload)
+        }
     }
 }
 
@@ -85,12 +99,16 @@ private struct FakeDiscordClient: DiscordClient {
         to endpoint: Endpoint,
         queries: [(String, String?)]
     ) async throws -> DiscordHTTPResponse {
-        DiscordHTTPResponse(
+        // Notify the mocked manager that the app has responded
+        // to the message by trying to send a message to Discord.
+        await FakeManager.shared.respond(to: endpoint, with: "")
+        
+        return DiscordHTTPResponse(
             host: "discord.com",
             status: .ok,
             version: .http2,
             headers: [:],
-            body: nil
+            body: testData(for: endpoint.urlSuffix).map { .init(data: $0) }
         )
     }
     
@@ -108,7 +126,7 @@ private struct FakeDiscordClient: DiscordClient {
             status: .ok,
             version: .http2,
             headers: [:],
-            body: nil
+            body: testData(for: endpoint.urlSuffix).map { .init(data: $0) }
         )
     }
 }
--- CODE/Tests/PennyBOTTests/GatewayProcessingTests.swift
+++ CODE/Tests/PennyBOTTests/GatewayProcessingTests.swift
@@ -8,12 +8,12 @@ import XCTest
 
 class GatewayProcessingTests: XCTestCase {
     
-    let manager = FakeManager.shared
+    var manager: FakeManager { .shared }
     
     override func setUp() async throws {
         Constants.coinServiceBaseUrl = "https://fake.com"
         BotFactory.makeBot = { _, _ in FakeManager.shared }
-        AWSClientFactory.makeClient = { 
+        AWSClientFactory.makeClient = {
             AWSClient(httpClientProvider: .shared(
                 FakeAWSHTTPClient(eventLoopGroup: $0)
             ))
@@ -24,6 +24,7 @@ class GatewayProcessingTests: XCTestCase {
         ServiceFactory.makeCoinService = { _, _ in
             FakeCoinService()
         }
+        FakeManager.shared = FakeManager()
         try await Penny.main()
     }
     
@@ -40,26 +41,28 @@ class GatewayProcessingTests: XCTestCase {
     }
     
     func testInteractionHandler() async throws {
-        let response = try await manager.sendAndAwaitResponse(
+        let response = try await self.manager.sendAndAwaitResponse(
             key: .linkInteraction,
-            endpoint: .createInteractionResponse(id: "1018162735864872971", token: "aW50ZXJhY3Rpb246MTAzMTExMjExMzk3ODA4OTUwMjpRVGVBVXU3Vk1XZ1R0QXpiYmhXbkpLcnFqN01MOXQ4T2pkcGRXYzRjUFNMZE9TQ3g4R3NyM1d3OGszalZGV2c3a0JJb2ZTZnluS3VlbUNBRDh5N2U3Rm00QzQ2SWRDMGJrelJtTFlveFI3S0RGbHBrZnpoWXJSNU1BV1RqYk5Xaw"),
-            as: InteractionResponse.self
+            endpoint: .editOriginalInteractionResponse(appId: "11111111", token: "aW50ZXJhY3Rpb246MTAzMTExMjExMzk3ODA4OTUwMjpRVGVBVXU3Vk1XZ1R0QXpiYmhXbkpLcnFqN01MOXQ4T2pkcGRXYzRjUFNMZE9TQ3g4R3NyM1d3OGszalZGV2c3a0JJb2ZTZnluS3VlbUNBRDh5N2U3Rm00QzQ2SWRDMGJrelJtTFlveFI3S0RGbHBrZnpoWXJSNU1BV1RqYk5Xaw"),
+            as: InteractionResponse.CallbackData.self
         )
         
-        let description = try XCTUnwrap(response.data?.embeds?.first?.description)
-        XCTAssertTrue(description.hasPrefix("<@950695294906007573> now has "))
-        XCTAssertTrue(description.hasSuffix(" coins!"))
+        let description = try XCTUnwrap(response.embeds?.first?.description)
+        XCTAssertEqual(description, "This command is still a WIP. Linking Discord with Discord ID 9123813923")
     }
     
     func testReactionHandler() async throws {
         let response = try await manager.sendAndAwaitResponse(
             key: .thanksReaction,
-            endpoint: .postCreateMessage(channelId: "441327731486097429"),
-            as: InteractionResponse.self
+            endpoint: .postCreateMessage(channelId: "966722151359057950"),
+            as: DiscordChannel.CreateMessage.self
         )
         
-        let description = try XCTUnwrap(response.data?.embeds?.first?.description)
-        XCTAssertTrue(description.hasPrefix("<@950695294906007573> now has "))
-        XCTAssertTrue(description.hasSuffix(" coins!"))
+        let description = try XCTUnwrap(response.embeds?.first?.description)
+        XCTAssertTrue(description.hasPrefix("""
+        <@290483761559240704> gave a coin to <@1030118727418646629>!
+        <@1030118727418646629> now has
+        """))
+        XCTAssertTrue(description.hasSuffix(" shiny coins."))
     }
 }
