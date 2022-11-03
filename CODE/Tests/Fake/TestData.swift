bf856784840d5b55b1358771b623443f0c98c4d8
Mahdi Bahrami <github@mahdibm.com>
2022-11-03 13:32:52 +0330

    Soto major update

--- CODE/Tests/Fake/TestData.swift
+++ CODE/Tests/Fake/TestData.swift
@@ -6,7 +6,7 @@ enum TestData {
         let currentDirectory = fileManager.currentDirectoryPath
         let path = currentDirectory + "/Tests/Resources/test_data.json"
         guard let data = fileManager.contents(atPath: path) else {
-            fatalError("Make sure you've set the custom working directory for the 'PennyBOT' scheme: https://docs.vapor.codes/getting-started/xcode/#custom-working-directory")
+            fatalError("Make sure you've set the custom working directory for the current scheme: https://docs.vapor.codes/getting-started/xcode/#custom-working-directory")
         }
         let object = try! JSONSerialization.jsonObject(with: data, options: [])
         return object as! [String: Any]
