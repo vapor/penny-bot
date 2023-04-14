df0d28e3cf4d995eae45dcf7c9842ab5e5c52d6b
Mahdi Bahrami <github@mahdibm.com>
2023-04-14 14:04:39 +0330

    nicer auto-pings title

--- CODE/Sources/PennyBOT/Handlers/InteractionHandler.swift
+++ CODE/Sources/PennyBOT/Handlers/InteractionHandler.swift
@@ -546,7 +546,7 @@ private enum ModalID: RawRepresentable {
         case let .autoPings(autoPingsMode, expressionMode):
             let autoPingsMode = autoPingsMode.rawValue.capitalized
             let expressionMode = expressionMode.UIDescription
-            return "\(autoPingsMode) \(expressionMode) auto-pings"
+            return "\(autoPingsMode) \(expressionMode) Auto-Pings"
         }
     }
 
