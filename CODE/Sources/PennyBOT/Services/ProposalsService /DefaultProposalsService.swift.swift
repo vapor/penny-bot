e1052203e76d2846df6b251f914567c282b811e1
Mahdi Bahrami <github@mahdibm.com>
2023-06-16 13:29:11 +0200

    fix github link conversion

--- CODE/Sources/PennyBOT/Services/ProposalsService /DefaultProposalsService.swift.swift
+++ CODE/Sources/PennyBOT/Services/ProposalsService /DefaultProposalsService.swift.swift
@@ -26,9 +26,17 @@ struct DefaultProposalsService: ProposalsService {
     }
 
     func getProposalContent(link: String) async throws -> String {
+        /// Converts a link like
+        /// https://github.com/apple/swift-evolution/blob/main/proposals/0401-remove-property-wrapper-isolation.md
+        /// to
+        /// https://raw.githubusercontent.com/apple/swift-evolution/main/proposals/0401-remove-property-wrapper-isolation.md
+        /// to get the raw content of the file instead of the Github web page.
         let link = link.replacingOccurrences(
             of: "github.com",
             with: "raw.githubusercontent.com"
+        ).replacingOccurrences(
+            of: "/blob/",
+            with: "/"
         )
         let response = try await httpClient.execute(
             .init(url: link),
