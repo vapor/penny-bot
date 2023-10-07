68c44e69da96bfec7b2b31a8744b2ca5769a4708
MahdiBM <github@mahdibm.com>
2023-10-07 12:26:27 +0330

    use a better endpoint

--- Sources/Penny/Services/StackoverflowService /DefaultSOService.swift
+++ Sources/Penny/Services/StackoverflowService /DefaultSOService.swift
@@ -10,6 +10,7 @@ struct DefaultSOService: SOService {
         let queries: [(String, String)] = [
             ("site", "stackoverflow"),
             ("tagged", "vapor"),
+            ("nottagged", "laravel"), /// Don't be a "laravel" question
             ("page", "1"),
             ("sort", "creation"),
             ("order", "desc"),
@@ -17,32 +18,28 @@ struct DefaultSOService: SOService {
             ("pagesize", "100"),
             ("key", Constants.StackOverflow.apiKey),
         ]
-        let url = "https://api.stackexchange.com/2.3/questions" + queries.makeForURLQuery()
+        let url = "https://api.stackexchange.com/2.3/search/advanced" + queries.makeForURLQueryUnchecked()
         let request = HTTPClientRequest(url: url)
         let response = try await httpClient.execute(request, deadline: .now() + .seconds(15))
         let buffer = try await response.body.collect(upTo: 1 << 25) /// 32 MB
         let questions = try decoder.decode(
             SOQuestions.self,
             from: buffer
-        ).items.filter {
-            /// Don't be a "laravel" question
-            !$0.tags.contains { $0.contains("laravel") }
-        }
+        ).items
         return questions
     }
 }
 
-extension [(String, String)] {
-    private func makeForURLQuery() -> String {
+private extension [(String, String)] {
+    /// Doesn't do url-query encoding.
+    /// Assumes the values are already safe.
+    func makeForURLQueryUnchecked() -> String {
         if self.isEmpty {
             return ""
         } else {
-            return "?" + self.compactMap { key, value -> String? in
-                let value = value.addingPercentEncoding(
-                    withAllowedCharacters: .urlQueryAllowed
-                ) ?? value
-                return "\(key)=\(value)"
-            }.joined(separator: "&")
+            return "?" + self
+                .compactMap({ "\($0)=\($1)" })
+                .joined(separator: "&")
         }
     }
 }
