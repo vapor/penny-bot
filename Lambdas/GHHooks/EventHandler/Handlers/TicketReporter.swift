8174ec1d240239b92c2570e775bbf8517873f90d
Mahdi Bahrami <github@mahdibm.com>
2024-01-06 18:47:01 +0330

    Another try to fix duplicate ticket reports on Discord (#155)

--- Lambdas/GHHooks/EventHandler/Handlers/TicketReporter.swift
+++ Lambdas/GHHooks/EventHandler/Handlers/TicketReporter.swift
@@ -11,7 +11,7 @@ struct TicketReporter {
 
     private static let ticketQueue = SerialProcessor()
 
-    let context: HandlerContext
+    var context: HandlerContext
     let embed: Embed
     let createdAt: Date
     let repoID: Int
@@ -24,15 +24,28 @@ struct TicketReporter {
         .reportingChannel(repoID: repoID, createdAt: createdAt)
     }
 
+    init(
+        context: HandlerContext,
+        embed: Embed,
+        createdAt: Date,
+        repoID: Int,
+        number: Int,
+        authorID: Int
+    ) {
+        self.context = context
+        self.embed = embed
+        self.createdAt = createdAt
+        self.repoID = repoID
+        self.number = number
+        self.authorID = authorID
+
+        self.context.logger[metadataKey: "repoID"] = .stringConvertible(repoID)
+        self.context.logger[metadataKey: "number"] = .stringConvertible(number)
+    }
+
     func reportCreation() async throws {
         if Configuration.userIDDenyList.contains(authorID) { return }
 
-        try await TicketReporter.ticketQueue.process(queueKey: ticketKey) {
-            try await _reportCreation()
-        }
-    }
-
-    private func _reportCreation() async throws {
         let response = try await context.discordClient.createMessage(
             channelId: self.channel.id,
             payload: .init(embeds: [embed])
@@ -45,14 +58,11 @@ struct TicketReporter {
         )
     }
 
-    func reportEdition() async throws {
+    /// - Parameter requiresPreexistingReport: Requires a report to have already been done about
+    /// the ticket. If not, this report process will be aborted.
+    func reportEdition(requiresPreexistingReport: Bool) async throws {
         if Configuration.userIDDenyList.contains(authorID) { return }
-        try await TicketReporter.ticketQueue.process(queueKey: ticketKey) {
-            try await _reportEdition()
-        }
-    }
 
-    private func _reportEdition() async throws {
         let messageID: MessageSnowflake
 
         do {
@@ -65,18 +75,17 @@ struct TicketReporter {
                 "messageID": "\(messageID)"
             ])
         } catch let error as DynamoMessageRepo.Errors where error == .unavailable {
-            context.logger.debug("Message is unavailable to edit", metadata: [
-                "repoID": .stringConvertible(repoID),
-                "number": .stringConvertible(number),
-            ])
+            context.logger.debug("Message is unavailable to edit")
             return
         } catch let error as DynamoMessageRepo.Errors where error == .notFound {
+            if requiresPreexistingReport {
+                context.logger.warning(
+                    "Didn't find a message id from the lookup repo, and the report requires a preexisting report"
+                )
+                return
+            }
             context.logger.debug(
-                "Didn't find a message id from the lookup repo, will send a new message",
-                metadata: [
-                    "repoID": .stringConvertible(repoID),
-                    "number": .stringConvertible(number),
-                ]
+                "Didn't find a message id from the lookup repo, will send a new message"
             )
 
             let response = try await context.discordClient.createMessage(
@@ -102,18 +111,15 @@ struct TicketReporter {
         case let .jsonError(jsonError) where jsonError.code == .unknownMessage:
             context.logger.debug(
                 "Discord says message id is unknown. Will mark as unavailable in DB.",
-                metadata: [
-                    "messageID": .stringConvertible(messageID),
-                    "repoID": .stringConvertible(repoID),
-                    "number": .stringConvertible(number),
-                ]
+                metadata: ["messageID": .stringConvertible(messageID)]
             )
             try await context.messageLookupRepo.markAsUnavailable(
                 repoID: repoID,
                 number: number
             )
-        default:
-            try response.guardSuccess()
+        case let .some(error):
+            throw error
+        default: break
         }
     }
 }
