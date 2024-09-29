6271f37505f1dd9c21ad4b72a59c9372d95af5b2
MahdiBM <github@mahdibm.com>
2024-09-29 09:05:35 +0330

    Try re-enable SO checker

--- Sources/Penny/SOChecker.swift
+++ Sources/Penny/SOChecker.swift
@@ -21,10 +21,6 @@ actor SOChecker {
     }
 
     nonisolated func run() {
-        #if !DEBUG
-        /// Cloudflare seems to be blocking us although we have a auth token.
-        return
-        #endif
         Task { [self] in
             if Task.isCancelled { return }
             do {
