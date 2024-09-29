fd4fcae5f45667b98b432c331a6da0bbfddfd05f
MahdiBM <github@mahdibm.com>
2024-09-29 09:20:34 +0330

    Revert "Try re-enable SO checker"
    This reverts commit 6271f37505f1dd9c21ad4b72a59c9372d95af5b2.

--- Sources/Penny/SOChecker.swift
+++ Sources/Penny/SOChecker.swift
@@ -21,6 +21,10 @@ actor SOChecker {
     }
 
     nonisolated func run() {
+        #if !DEBUG
+        /// Cloudflare seems to be blocking us although we have a auth token.
+        return
+        #endif
         Task { [self] in
             if Task.isCancelled { return }
             do {
