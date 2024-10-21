4f4b88ebab1148a974fd747023b7d24e5b104ff2
MahdiBM <github@mahdibm.com>
2024-10-21 16:10:18 +0330

    better logging

--- Sources/Penny/WaiterService.swift
+++ Sources/Penny/WaiterService.swift
@@ -1,12 +1,15 @@
+import Logging
 import ServiceLifecycle
 import Shared
 
 struct WaiterService<UnderlyingService: Service>: Service {
     private let underlying: UnderlyingService
+    private let logger: Logger
     private let canRun: @Sendable () async -> Void
 
     init(
         underlyingService: UnderlyingService,
+        logger: Logger = Logger(label: _typeName(Self.self)),
         processingOn backgroundProcessor: BackgroundProcessor,
         passingContinuationWith passContinuation: @Sendable @escaping (CheckedContinuation<Void, Never>) async -> Void
     ) {
@@ -18,10 +21,20 @@ struct WaiterService<UnderlyingService: Service>: Service {
                 }
             }
         }
+        var logger = logger
+        logger[metadataKey: "underlyingServiceType"] = .string(_typeName(UnderlyingService.self))
+        self.logger = logger
     }
 
     func run() async throws {
+        defer {
+            self.logger.debug("Underlying service exited")
+        }
+
+        self.logger.trace("Will wait until we can run the underlying service")
         await self.canRun()
+
+        self.logger.debug("Will run the underlying service")
         try await self.underlying.run()
     }
 }
