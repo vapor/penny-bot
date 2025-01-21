b628511ccb434d1d4cfeea03093f0cd8f64f820a
MahdiBM <github@mahdibm.com>
2025-01-21 17:12:18 +0330

    minor

--- Lambdas/GHHooks/EventHandler/Handlers/PRHandler.swift
+++ Lambdas/GHHooks/EventHandler/Handlers/PRHandler.swift
@@ -132,7 +132,7 @@ struct PRHandler {
         let isIgnorable = self.pr.isIgnorableDoNotMergePR
         if isIgnorable {
             logger.info(
-                "Ignoring PR",
+                "PR is ignorable",
                 metadata: [
                     "pr": "\(self.pr)"
                 ]
