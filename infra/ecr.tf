e320f11de401d892a898871c28445f551bb93e5e
MahdiBM <github@mahdibm.com>
2026-07-26 18:45:52 +0330

    Improve ECR settings + ensure CI doesn't fail on immutable tag re-pushes

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,12 +1,33 @@
 resource "aws_ecr_repository" "penny_bot_discord_image" {
   name                 = "penny-bot-discord-image"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
   }
 
   encryption_configuration {
     encryption_type = "AES256"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "penny_bot_discord_image" {
+  repository = aws_ecr_repository.penny_bot_discord_image.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only the 30 most recent images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
