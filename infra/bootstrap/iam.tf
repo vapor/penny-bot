c3ac17d452366753a83d6d1d3bf62052acd6ea21
MahdiBM <github@mahdibm.com>
2026-07-28 21:08:50 +0330

    Tighter permissions regarding dynamo-db

--- infra/bootstrap/iam.tf
+++ infra/bootstrap/iam.tf
@@ -53,7 +53,6 @@ data "aws_iam_policy_document" "ecs_task_s3_caches" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:DeleteObject",
-      "s3:ListBucket",
     ]
     resources = ["${module.constants.bucket_arns.penny_caches}/*"]
   }
@@ -72,7 +71,7 @@ data "aws_iam_policy_document" "lambda_assume" {
 
     principals {
       type        = "Service"
-      identifiers = ["lambda.amazonaws.com", "apigateway.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com"]
     }
   }
 }
@@ -93,7 +92,6 @@ data "aws_iam_policy_document" "lambda" {
     sid    = "Logs"
     effect = "Allow"
     actions = [
-      "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents",
     ]
@@ -104,21 +102,31 @@ data "aws_iam_policy_document" "lambda" {
   }
 
   statement {
-    sid    = "DynamoDB"
-    effect = "Allow"
-    actions = [
-      "dynamodb:GetItem",
-      "dynamodb:Query",
-      "dynamodb:PutItem",
-      "dynamodb:UpdateItem",
-    ]
-    resources = flatten([
-      for arn in [
-        module.constants.table_arns.penny_user,
-        module.constants.table_arns.penny_coin,
-        module.constants.table_arns.ghhooks_message_lookup,
-      ] : [arn, "${arn}/*"]
-    ])
+    sid       = "DynamoDBPennyUser"
+    effect    = "Allow"
+    actions   = ["dynamodb:PutItem", "dynamodb:UpdateItem"]
+    resources = [module.constants.table_arns.penny_user]
+  }
+
+  statement {
+    sid       = "DynamoDBPennyUserIndexes"
+    effect    = "Allow"
+    actions   = ["dynamodb:Query"]
+    resources = ["${module.constants.table_arns.penny_user}/index/*"]
+  }
+
+  statement {
+    sid       = "DynamoDBPennyCoin"
+    effect    = "Allow"
+    actions   = ["dynamodb:PutItem"]
+    resources = [module.constants.table_arns.penny_coin]
+  }
+
+  statement {
+    sid       = "DynamoDBGHHooksMessageLookup"
+    effect    = "Allow"
+    actions   = ["dynamodb:Query", "dynamodb:UpdateItem"]
+    resources = [module.constants.table_arns.ghhooks_message_lookup]
   }
 
   statement {
